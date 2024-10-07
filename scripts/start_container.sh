8a26fc0f1511bc3e6f407c7620f4614335c65bfc
Debarshi Mullick <dedarshimullick7@gmail.com>
2024-10-07 18:43:31 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull debarshi11/simple-python-flask-app
+sudo docker pull debarshi11/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 adebarshi11/simple-python-flask-app
