5b387686c47b1e792b710c4b515b6d3a8e6bb30f
Debarshi Mullick <dedarshimullick7@gmail.com>
2024-10-07 19:41:26 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -2,4 +2,8 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid=$(docker ps -q)  # Get only running container IDs
+if [ -n "$containerid" ]; then  # Check if containerid is not empty
+    docker stop "$containerid"  # Stop the container
+    docker rm -f "$containerid"  # Force remove the container
+fi
